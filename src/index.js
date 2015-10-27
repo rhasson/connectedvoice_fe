027e3efa623be78491a4aa9d01b16601d25e8fd8
rhasson <rhasson@gmail.com>
2015-10-27 12:21:06 -0400

    setup middleware to verify twilio message signature

--- src/index.js
+++ src/index.js
@@ -2,4 +2,40 @@
 
 import Config from '../config.json';
 import Request from 'request-promise';
+import Twilio from 'twilio';
+import Restify from 'restify';
+import Verify from './libs/verify_twilio.js';
+import {voiceCallHandler, smsCallHandler} from './libs/route_handlers';
 
+let server = Restify.createServer();
+
+server.use(Restify.queryParser());
+server.use(Restify.gzipResponse());
+server.use(Restify.bodyParser());
+
+/*
+* Middleware to verify Twilio message signature
+* If successul pass the request to the handlers, if failure return a 403 error
+*/
+server.use(function(req, reply, next) {
+	Verify.isValidSignature(req)
+	.then(function(isValid) {
+		if (isValid) return next();
+	})
+	.catch(function(err) {
+		reply.send(403, 'Failed to verify twilio signature');
+		reply.end();
+	});
+});
+
+server.post('/actions/v0/:id/voice.xml', voiceCallHandler);
+server.post('/actions/v0/:id/sms.xml', smsCallHandler);
+server.post('/actions/v0/:id/status', voiceCallHandler);
+server.post('/actions/v0/:id/action', voiceCallHandler);
+server.post('/actions/v0/:id/action/:index', voiceCallHandler);
+server.post('/actions/v0/:id/dequeue', voiceCallHandler);
+server.post('/actions/v0/:id/wait/:index', voiceCallHandler);
+
+server.listen(9000, function() {
+	console.log('ConnectedVoice FrontEnd Server Started - ', new Date());
+});
