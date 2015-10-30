1822b244e22829172874ba3927a905bb6d7d6a91
rhasson <rhasson@gmail.com>
2015-10-30 11:02:48 -0400

    built:

--- build/libs/route_handlers.js
+++ build/libs/route_handlers.js
@@ -49,27 +49,12 @@ function voiceCallHandler(req, reply, next) {
 				throw new Error('Missing request parameters');
 
 			case 9:
-				if (!(body === undefined)) {
-					context$1$0.next = 13;
-					break;
-				}
-
-				reply.send(200);
-				context$1$0.next = 17;
-				break;
-
-			case 13:
-				body = new Buffer(body, 'base64').toString('utf8');
-				reply.send(200, body, { 'content-type': 'application/xml' });
+				if (body === undefined) reply.send(200);else reply.send(200, body, { 'content-type': 'application/xml' }); //may need to: unescape(body)
 				reply.end();
 				return context$1$0.abrupt('return', next());
 
-			case 17:
-				context$1$0.next = 25;
-				break;
-
-			case 19:
-				context$1$0.prev = 19;
+			case 14:
+				context$1$0.prev = 14;
 				context$1$0.t0 = context$1$0['catch'](0);
 
 				console.log('voiceCallHandler Error ', context$1$0.t0.message);
@@ -77,11 +62,11 @@ function voiceCallHandler(req, reply, next) {
 				reply.end();
 				return context$1$0.abrupt('return');
 
-			case 25:
+			case 20:
 			case 'end':
 				return context$1$0.stop();
 		}
-	}, null, this, [[0, 19]]);
+	}, null, this, [[0, 14]]);
 }
 
 function smsCallHandler(req, reply, next) {
